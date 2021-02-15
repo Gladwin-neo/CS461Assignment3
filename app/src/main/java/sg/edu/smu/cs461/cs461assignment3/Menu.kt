71b1f89fbd73eebff50039b1aa0706466a1cf8d0
Gladwin <gladwin.neo.2018@sis.smu.edu.sg>
2021-02-15 23:40:08 +0800

    Update Menu.kt

--- app/src/main/java/sg/edu/smu/cs461/cs461assignment3/Menu.kt
+++ app/src/main/java/sg/edu/smu/cs461/cs461assignment3/Menu.kt
@@ -12,21 +12,25 @@ class Menu : AppCompatActivity() {
         setContentView(R.layout.activity_menu)
     }
 
+    // bringing users to the main page to see user A's profile
     fun userA(view: View) {
         val it = Intent(this, UserA::class.java)
         startActivityForResult(it, REQ_CODE)
     }
 
+    // bringing users to the main page to see user B's profile
     fun userB(view: View) {
         val it = Intent(this, UserB::class.java)
         startActivityForResult(it, REQ_CODE)
     }
 
+    // bringing users to the main page to see user C's profile
     fun userC(view: View) {
         val it = Intent(this, UserC::class.java)
         startActivityForResult(it, REQ_CODE)
     }
 
+    // bringing users to the main page to see user D's profile
     fun userD(view: View) {
         val it = Intent(this, UserD::class.java)
         startActivityForResult(it, REQ_CODE)
