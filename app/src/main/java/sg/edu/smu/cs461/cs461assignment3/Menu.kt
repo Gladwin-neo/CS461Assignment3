320576666b61352d19dcf3ad3394fcf37e8a5943
Gladwin <gladwin.neo.2018@sis.smu.edu.sg>
2021-02-15 23:32:56 +0800

    other user profile

--- app/src/main/java/sg/edu/smu/cs461/cs461assignment3/Menu.kt
+++ app/src/main/java/sg/edu/smu/cs461/cs461assignment3/Menu.kt
@@ -1,11 +1,34 @@
 package sg.edu.smu.cs461.cs461assignment3
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class Menu : AppCompatActivity() {
+    private val REQ_CODE = 1234
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
+    }
+
+    fun userA(view: View) {
+        val it = Intent(this, UserA::class.java)
+        startActivityForResult(it, REQ_CODE)
+    }
+
+    fun userB(view: View) {
+        val it = Intent(this, UserB::class.java)
+        startActivityForResult(it, REQ_CODE)
+    }
+
+    fun userC(view: View) {
+        val it = Intent(this, UserC::class.java)
+        startActivityForResult(it, REQ_CODE)
+    }
+
+    fun userD(view: View) {
+        val it = Intent(this, UserD::class.java)
+        startActivityForResult(it, REQ_CODE)
     }
 }
---
+++ app/src/main/java/sg/edu/smu/cs461/cs461assignment3/UserD.kt
@@ -0,0 +1,11 @@
+package sg.edu.smu.cs461.cs461assignment3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class UserD : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_d)
+    }
+}
