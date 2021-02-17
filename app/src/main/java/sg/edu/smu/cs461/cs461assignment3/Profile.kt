03e551b4ac49efb158cc9e65e7163d5743def3dc
Gladwin <gladwin.neo.2018@sis.smu.edu.sg>
2021-02-17 17:31:12 +0800

    Update Profile.kt

--- app/src/main/java/sg/edu/smu/cs461/cs461assignment3/Profile.kt
+++ app/src/main/java/sg/edu/smu/cs461/cs461assignment3/Profile.kt
@@ -1,20 +1,19 @@
 package sg.edu.smu.cs461.cs461assignment3
 
-import android.app.Activity
 import android.content.Intent
-import android.content.pm.PackageManager
 import android.graphics.Bitmap
-import androidx.appcompat.app.AppCompatActivity
+import android.net.Uri
 import android.os.Bundle
 import android.provider.MediaStore
+import android.util.Log
 import android.view.View
 import android.widget.EditText
 import android.widget.ImageView
-import androidx.core.app.ActivityCompat
-import java.util.jar.Manifest
+import androidx.appcompat.app.AppCompatActivity
 
 class Profile : AppCompatActivity(){
     private val REQ_CODE = 1234
+    private val REQ_CODE_PROFILE = 1235
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -40,21 +39,32 @@ class Profile : AppCompatActivity(){
         pickImageFileIntent.action = Intent.ACTION_GET_CONTENT
 
         val pickGalleryImageIntent = Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
-
         val captureCameraImageIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
 
         val pickTitle = "Capture from camera or Select from gallery the Profile photo"
         val chooserIntent = Intent.createChooser(pickImageFileIntent, pickTitle)
-        chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, arrayOf(captureCameraImageIntent, pickGalleryImageIntent))
-        startActivityForResult(chooserIntent, 101)
+        chooserIntent.putExtra(
+            Intent.EXTRA_INITIAL_INTENTS, arrayOf(
+                captureCameraImageIntent,
+                pickGalleryImageIntent
+            )
+        )
+        chooserIntent.action.
+        startActivityForResult(chooserIntent, 1)
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if(requestCode==101){
-            var pic: Bitmap? = data?.getParcelableExtra<Bitmap>("data")
-            val profileImage = findViewById<ImageView>(R.id.profileImage)
-            profileImage.setImageBitmap(pic)
+    override fun onActivityResult(requestCode: Int, resultCode: Int, imageReturnedIntent: Intent?) {
+        super.onActivityResult(requestCode, resultCode, imageReturnedIntent)
+        val profileImage = findViewById<ImageView>(R.id.profileImage)
+        when (requestCode) {
+            0 -> if (resultCode == RESULT_OK) {
+                var pic: Bitmap? = imageReturnedIntent?.getParcelableExtra<Bitmap>("data")
+                profileImage.setImageBitmap(pic)
+            }
+            1 -> if (resultCode == RESULT_OK) {
+                val selectedImage: Uri? = imageReturnedIntent?.data
+                profileImage.setImageURI(selectedImage)
+            }
         }
     }
 }
