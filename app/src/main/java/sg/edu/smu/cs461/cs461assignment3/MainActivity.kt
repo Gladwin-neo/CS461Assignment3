0a2db83cb12d8793de4daea81a6c141cf9f9bbed
Gladwin <gladwin.neo.2018@sis.smu.edu.sg>
2021-02-22 17:09:19 +0800

    minor changes

--- app/src/main/java/sg/edu/smu/cs461/cs461assignment3/MainActivity.kt
+++ app/src/main/java/sg/edu/smu/cs461/cs461assignment3/MainActivity.kt
@@ -3,6 +3,7 @@ package sg.edu.smu.cs461.cs461assignment3
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.TextView
 
@@ -11,6 +12,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // getting the name the was saved alr
+        val name = getSharedPreferences("Name", 0).getString("Name", null)
+        Log.i("test",name)
+        val title = findViewById<TextView>(R.id.textView)
+
+        val originalTitle = title.text.toString()
+        title.text = originalTitle + name
     }
 
     // bringing users to the create profile page
