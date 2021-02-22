5580ac2a084ce801b69ae211bb32b52d77f7ab23
Gladwin <gladwin.neo.2018@sis.smu.edu.sg>
2021-02-22 13:04:50 +0800

    ratings

--- app/src/main/java/sg/edu/smu/cs461/cs461assignment3/UserC.kt
+++ app/src/main/java/sg/edu/smu/cs461/cs461assignment3/UserC.kt
@@ -1,11 +1,21 @@
 package sg.edu.smu.cs461.cs461assignment3
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.RatingBar
 
 class UserC : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_c)
+//        loadData()
+    }
+
+    private fun loadData(){
+        val sharedPreference = getPreferences(Context.MODE_PRIVATE)
+        val rating = sharedPreference.getFloat("numStars", 3f)
+        val ratingBar = findViewById<RatingBar>(R.id.rating)
+        ratingBar.rating = rating
     }
 }
--- app/src/main/java/sg/edu/smu/cs461/cs461assignment3/UserD.kt
+++ app/src/main/java/sg/edu/smu/cs461/cs461assignment3/UserD.kt
@@ -1,11 +1,21 @@
 package sg.edu.smu.cs461.cs461assignment3
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.RatingBar
 
 class UserD : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_d)
+//        loadData()
+    }
+
+    private fun loadData(){
+        val sharedPreference = getPreferences(Context.MODE_PRIVATE)
+        val rating = sharedPreference.getFloat("numStars", 4f)
+        val ratingBar = findViewById<RatingBar>(R.id.rating)
+        ratingBar.rating = rating
     }
 }
